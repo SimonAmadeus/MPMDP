03081d8f8f79a0d38e3922033c23b4dd45b57f6e
Simon Alberti <81336688+SimonAmadeus@users.noreply.github.com>
2023-06-19 11:37:43 +0200

    New version with many changes available

--- source/core/functions/calc_T.jl
+++
@@ -1,5 +0,0 @@
-function calc_inst_temp(particle_vec::Vector{Particle}, K_tot::Float64)
-    dim::Int64 = 3
-    T::Float64 = K_tot * 2 / (length(particle_vec) * dim - dim)
-    return T
-end
--- source/interactions/non_bonded/no_non_bonded.jl
+++
@@ -1,3 +0,0 @@
-function apply_non_bonded_interactions!(args::System, ::Any, particle_vec::Vector{Particle}, c_l::Int64, force_vec::Vector{Force}, energy_vec::Vector{Float64}, stress::Vector{Float64}, mesh::Mesh, ::No_Non_Bonded) 
-    return energy, stress
-end
